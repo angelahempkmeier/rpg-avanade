9862b0db0e8c438a45f55829dd0373951679c238
Angela Hempkmeier <aggsdpz@gmail.com>
2023-09-09 14:16:23 -0300

    initial commit

---
+++ rpg/src/main/java/com/avanade/rpg/RpgApplication.java
@@ -0,0 +1,17 @@
+package com.avanade.rpg;
+
+import com.avanade.rpg.characters.heroes.Barbarian;
+import com.avanade.rpg.characters.heroes.Warrior;
+import com.avanade.rpg.entities.Character;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpgApplication.class, args);
+
+    }
+
+}
