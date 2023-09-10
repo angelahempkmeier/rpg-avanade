5f08afde1165102cbdc0a67fb34aa38f9541ae98
Angela Hempkmeier <aggsdpz@gmail.com>
2023-09-09 23:11:41 -0300

    feat(repositories): implementing Character Repository

---
+++ rpg/src/main/java/com/avanade/rpg/repositories/CharacterRepository.java
@@ -0,0 +1,9 @@
+package com.avanade.rpg.repositories;
+
+import com.avanade.rpg.entities.Character;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CharacterRepository extends JpaRepository<Character, Long> {
+}
