fc6179383740cc20336723a050e6441268b2478b
Angela Hempkmeier <aggsdpz@gmail.com>
2023-09-09 23:14:05 -0300

    feat(test): test package & default configs

---
+++ rpg/src/test/java/com/avanade/rpg/RpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avanade.rpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
