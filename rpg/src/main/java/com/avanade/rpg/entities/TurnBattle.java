8b13418490562d3112f13987de830731bf8e2a46
Angela Hempkmeier <aggsdpz@gmail.com>
2023-09-11 22:21:03 -0300

    refactor(TurnBattle): persistence changes to accept duplicated characters keys

--- rpg/src/main/java/com/avanade/rpg/entities/TurnBattle.java
+++ rpg/src/main/java/com/avanade/rpg/entities/TurnBattle.java
@@ -1,5 +1,6 @@
 package com.avanade.rpg.entities;
 
+import com.avanade.rpg.enums.TurnStatus;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,10 +14,18 @@ public class TurnBattle {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @ManyToOne(optional = true)
+    @JoinColumn(name = "attacker_id")
+    private Character attacker;
+    @ManyToOne(optional = true)
+    @JoinColumn(name = "defender_id")
+    private Character defender;
     private Integer turn;
     private Integer attack;
     private Integer defense;
     private Integer damage;
+    @Enumerated(EnumType.STRING)
+    private TurnStatus status;
 
     @ManyToOne
     @JoinColumn(name = "game_id")
