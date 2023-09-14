b6f6fe2a4c534f0b56ac44ea75d5acfd7679b7fb
Angela Hempkmeier <aggsdpz@gmail.com>
2023-09-14 19:14:23 -0300

    refactor(controller): code smell & refactoring methods for character controller

--- rpg/src/main/java/com/avanade/rpg/controllers/CharacterController.java
+++ rpg/src/main/java/com/avanade/rpg/controllers/CharacterController.java
@@ -1,17 +1,19 @@
 package com.avanade.rpg.controllers;
 
+import com.avanade.rpg.dto.characters.CharacterEditRequestDTO;
+import com.avanade.rpg.dto.characters.CharacterRequestDTO;
+import com.avanade.rpg.dto.characters.CharacterResponseDTO;
 import com.avanade.rpg.services.CharacterService;
-import com.avanade.rpg.dto.CharacterRequestDTO;
-import com.avanade.rpg.dto.CharacterResponseDTO;
-import com.avanade.rpg.dto.CharactersAvailableDTO;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("characters")
 public class CharacterController {
-
     private CharacterService characterService;
 
     public CharacterController(CharacterService characterService) {
@@ -19,7 +21,7 @@ public class CharacterController {
     }
 
     @PostMapping
-    public ResponseEntity<CharacterResponseDTO> create(@RequestBody CharacterRequestDTO dto){
+    public ResponseEntity<CharacterResponseDTO> create(@Valid @RequestBody CharacterRequestDTO dto){
         CharacterResponseDTO response = characterService.create(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
@@ -27,10 +29,25 @@ public class CharacterController {
     @GetMapping("/{id}")
     public ResponseEntity<CharacterResponseDTO> findById(@PathVariable Long id){
         CharacterResponseDTO character = characterService.findById(id);
-        return ResponseEntity.ok().body(character);
+        return ResponseEntity.status(HttpStatus.OK).body(character);
     }
 
-    public CharactersAvailableDTO getCharactersAvailable(){
-        return null;
+    @GetMapping
+    public ResponseEntity<List<CharacterResponseDTO>> findAll(){
+        List<CharacterResponseDTO> characters = characterService.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(characters);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CharacterResponseDTO> update(@Valid @RequestBody CharacterEditRequestDTO dto,
+                                                       @PathVariable Long id){
+        CharacterResponseDTO characterResponseDTO = characterService.update(dto, id);
+        return ResponseEntity.status(HttpStatus.OK).body(characterResponseDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        characterService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
